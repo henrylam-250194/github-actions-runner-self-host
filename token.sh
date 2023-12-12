3a433c46f2ca18d43089edb6ed7805a9665e6b24
henrylam-250194 <141704012+henrylam-250194@users.noreply.github.com>
2023-12-12 11:33:38 +0700

    BugFix/Dockerfile-issues

--- token.sh
+++ token.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
-registration_url="https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token" # need to be change to ORG runner url it you want to change to ORG self runner
+registration_url="https://api.github.com/repos/$OWNER/$REPO/actions/runners/registration-token"
 echo "Requesting registration URL at '$registration_url'"
-
-# API call to generate the registration token for a GitHub repository runner
 
 response=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" $registration_url)
 
 export RUNNER_TOKEN=$(echo $response | jq .token --raw-output)
-# # Extract the token from the response
 
 echo "Runner registration token: $RUNNER_TOKEN"
 
-# cd /home/runner/actions-runner
-./config.sh --url https://github.com/$OWNER/$REPO --token $RUNNER_TOKEN --name $(hostname)
+./config.sh \
+    --name $(hostname) \
+    --token $RUNNER_TOKEN \
+    --url https://github.com/$OWNER/$REPO \
+    --work "/work" \
+    --unattended \
+    --replace
+
+remove() {
+    ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
+}
 
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
